17adc45afd9bad2c72205eef56c4a8295fca1ade
bkonkle <brandon@konkle.us>
2012-12-06 15:23:52 -0700

    Add a Category model to demonstrate relationships

--- todos/todo/admin.py
+++ todos/todo/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
-from todos.todo.models import Todo
+from todos.todo.models import Category, Todo
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Category, CategoryAdmin)
 
 
 class TodoAdmin(admin.ModelAdmin):
     pass
-
-
 admin.site.register(Todo, TodoAdmin)
--- todos/todo/models.py
+++ todos/todo/models.py
@@ -1,11 +1,22 @@
 from django.db import models
 
 
+class Category(models.Model):
+    title = models.CharField(max_length=300)
+
+    class Meta:
+        verbose_name_plural = 'categories'
+
+    def __unicode__(self):
+        return self.title
+
+
 class Todo(models.Model):
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=300)
     order = models.IntegerField(null=True, blank=True)
     done = models.BooleanField(default=False)
     owner = models.ForeignKey('auth.User')
+    category = models.ForeignKey(Category, null=True, blank=True)
 
     def __unicode__(self):
         return self.title
