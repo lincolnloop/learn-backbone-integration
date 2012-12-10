6d1b032a3c66053aeaf2219c89637f942d3e6b0a
bkonkle <brandon@konkle.us>
2012-12-10 14:04:04 -0700

    Use TemplateView instead of direct_to_template
    Conflicts:
    	todos/todo/urls.py

--- todos/todo/urls.py
+++ todos/todo/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from django.views.generic.simple import direct_to_template
+from django.views.generic import TemplateView
 
 from todos.todo.api import CategoryResource, TodoResource
 
@@ -13,7 +13,7 @@ category_resource = CategoryResource()
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
 
-    url(r'^$', direct_to_template, {'template': 'index.html'}),
+    url(r'^$', TemplateView.as_view(template_name="index.html")),
 
     (r'^', include(category_resource.urls)),
     (r'^', include(todo_resource.urls)),
