9f242203f34ac59b87c32cc92ed2fd89915afa0e
bkonkle <brandon@konkle.us>
2012-12-10 14:04:04 -0700

    Use TemplateView instead of direct_to_template

--- todos/todo/urls.py
+++ todos/todo/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from django.views.generic.simple import direct_to_template
+from django.views.generic import TemplateView
 
 from todos.todo.views import CategoryView, TodoView
 
@@ -14,5 +14,5 @@ urlpatterns = patterns('',
         name='categories'),
     url(r'^todos(?:/(?P<pk>\d+))?/$', TodoView.as_view(), name='todos'),
 
-    url(r'^$', direct_to_template, {'template': 'index.html'}),
+    url(r'^$', TemplateView.as_view(template_name="index.html")),
 )
